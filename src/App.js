b4cf02807bb1bbab35c06a7c852cdb623775a554
manojmksmanu <manoj2022019@gmail.com>
2024-02-12 21:57:07 +0530

    set up all routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Details from './pages/Detail';
+import AddEditBlog from './pages/AddEditBlog';
+import NotFound from './pages/NotFound';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { ToastContainer } from "react-toastify"
 function App() {
   return (
     <div className="App">
+      <ToastContainer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/create" element={<AddEditBlog />} />
+          <Route path="/update/:id" element={<AddEditBlog />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
 
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+
+      <h2 className=''>Home</h2>
+    </div>
   )
 }
 
